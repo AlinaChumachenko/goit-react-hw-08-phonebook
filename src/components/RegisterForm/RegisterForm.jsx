26300924ee9fb2001b4d9f73457c8d264596e55f
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-01-07 15:15:31 +0200

    registerform

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -18,41 +18,19 @@ export const RegisterForm = () => {
     form.reset();
   };
   return (
-    <div className={css.conteiner}>
+    <div className={css.wrapper}>
       <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
         <label className={css.label}>
           Username
-          <input
-            className={css.input}
-            type="text"
-            name="name"
-            // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            placeholder="Enter name ..."
-            required
-          />
+          <input className={css.input} type="text" name="name" />
         </label>
         <label className={css.label}>
           Email
-          <input
-            className={css.input}
-            type="email"
-            name="email"
-            // pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/."
-            placeholder="Enter email ..."
-            required
-          />
+          <input className={css.input} type="email" name="email" />
         </label>
         <label className={css.label}>
           Password
-          <input
-            className={css.input}
-            type="password"
-            name="password"
-            // pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-            // title="Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters."
-            placeholder="Enter password ..."
-            required
-          />
+          <input className={css.input} type="password" name="password" />
         </label>
         <button className={css.button} type="submit">
           Register
