e603c37f23dcdb6ca36858aee3e9628ad2dae857
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2024-01-08 12:36:40 +0200

    rejister

--- src/redux/authorization/operations.js
+++ src/redux/authorization/operations.js
@@ -13,57 +13,111 @@ const clearAuthHeader = () => {
 
 export const register = createAsyncThunk(
   'auth/register',
-  async (credentials, { rejectWithValue }) => {
+  async (credentials, thunkAPI) => {
     try {
-      const { data } = await axios.post('users/signup', credentials);
-      setAuthHeader(data.token);
-      return data;
-    } catch (e) {
-      return rejectWithValue(e);
+      const responce = await axios.post('users/signup', credentials);
+      setAuthHeader(responce.data.token);
+      return responce.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
 export const logIn = createAsyncThunk(
   'auth/login',
-  async (credentials, { rejectWithValue }) => {
+  async (credentials, thunkAPI) => {
     try {
-      const { data } = await axios.post('users/login', credentials);
-      setAuthHeader(data.token);
-      return data;
-    } catch (e) {
-      return rejectWithValue(e);
+      const responce = await axios.post('users/login', credentials);
+      setAuthHeader(responce.data.token);
+      return responce.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
-export const logOut = createAsyncThunk(
-  'auth/logout',
-  async (_, { rejectWithValue }) => {
-    try {
-      await axios.post('users/logout');
-      clearAuthHeader();
-    } catch (e) {
-      return rejectWithValue(e);
-    }
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await axios.post('users/logout');
+    clearAuthHeader();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
   }
-);
+});
 
 export const refreshUser = createAsyncThunk(
   'auth/refresh',
-  async (_, { rejectWithValue }) => {
-    const state = rejectWithValue.getState();
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
     const persistedToken = state.auth.token;
     if (persistedToken === null) {
-      return rejectWithValue('Unable to fetch user');
+      return thunkAPI.rejectWithValue('Unable to fetch user');
     }
 
     try {
       setAuthHeader(persistedToken);
-      const { data } = await axios.get('/users/current');
-      return data;
-    } catch (e) {
-      return rejectWithValue(e);
+      const responce = await axios.get('/users/current');
+      return responce.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
+
+// export const register = createAsyncThunk(
+//   'auth/register',
+//   async (credentials, { rejectWithValue }) => {
+//     try {
+//       const { data } = await axios.post('users/signup', credentials);
+//       setAuthHeader(data.token);
+//       return data;
+//     } catch (e) {
+//       return rejectWithValue(e);
+//     }
+//   }
+// );
+
+// export const logIn = createAsyncThunk(
+//   'auth/login',
+//   async (credentials, { rejectWithValue }) => {
+//     try {
+//       const { data } = await axios.post('users/login', credentials);
+//       setAuthHeader(data.token);
+//       return data;
+//     } catch (e) {
+//       return rejectWithValue(e);
+//     }
+//   }
+// );
+
+// export const logOut = createAsyncThunk(
+//   'auth/logout',
+//   async (_, { rejectWithValue }) => {
+//     try {
+//       await axios.post('users/logout');
+//       clearAuthHeader();
+//     } catch (e) {
+//       return rejectWithValue(e);
+//     }
+//   }
+// );
+
+// export const refreshUser = createAsyncThunk(
+//   'auth/refresh',
+//   async (_, { rejectWithValue }) => {
+//     const state = rejectWithValue.getState();
+//     const persistedToken = state.auth.token;
+//     if (persistedToken === null) {
+//       return rejectWithValue('Unable to fetch user');
+//     }
+
+//     try {
+//       setAuthHeader(persistedToken);
+//       const { data } = await axios.get('/users/current');
+//       return data;
+//     } catch (e) {
+//       return rejectWithValue(e);
+//     }
+//   }
+// );
